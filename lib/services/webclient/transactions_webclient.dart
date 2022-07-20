6e9110235d4acd2f2f5613b5e3e55e5bc5c152f0
dripivetta <dripivetta@hotmail.com>
2022-07-20 16:42:42 -0300

    WebClient-Interceptors

--- lib/services/webclient/transactions_webclient.dart
+++ lib/services/webclient/transactions_webclient.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart';
 class TransactionWebClient {
   Future<List<Transaction>> findAll() async {
     final Response response = await client
-        .get(Uri.parse(baseUrl));
+        .get(Uri.parse(baseUrl)).timeout(const Duration(seconds: 30));
     final List<dynamic> decodedJson = jsonDecode(response.body);
     return decodedJson
         .map((dynamic json) => Transaction.fromJson(json))
