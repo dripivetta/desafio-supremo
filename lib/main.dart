e9afe427562a55e37f413b63c5f2552c28aec41f
Andressa28 <andressacportela@gmail.com>
2022-07-22 14:32:08 -0300

    Comprovante

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:desafio/presentation/pages/comprovante_details_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,17 +8,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
-      //home: const MyHomePage(title: 'Desafio Supreme'),
+      // home: const MyHomePage(title: 'Desafio Supreme'),
+
+      home: const ComprovanteDetailsPage(
+        amount: null,
+        authentication: '',
+        createdAt: '',
+        description: '',
+        to: '',
+        tTipe: '',
+      ),
     );
   }
 }
-
